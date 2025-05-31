53f2f6b734b9e8748bb8763365be4bdb6139200b
AnaCDiass <anaclaradiasq@gmail.com>
2025-05-31 20:52:46 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
+from django.views import View
+#from django.contrib import messages
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
-
-class CidadesView(View):
+    
+class CursosView(View):
     def get(self, request, *args, **kwargs):
-        cidades = Cidade.objects.all()
-        return render(request, 'cidade.html', {'cidades': cidades})
+        cursos = Curso.objects.all()
+        return render(request, 'curso.html', {'cursos': cursos})
 
-class OcupacoesView(View):
+class MatriculasView(View):
     def get(self, request, *args, **kwargs):
-        ocupacoes = Ocupacao.objects.all()
-        return render(request, 'ocupacao.html', {'ocupacoes': ocupacoes})
+        matriculas = Matricula.objects.all()
+        return render(request, 'matricula.html', {'matriculas': matriculas})
 
 class PessoasView(View):
     def get(self, request, *args, **kwargs):
@@ -26,62 +27,28 @@ class InstituicoesView(View):
         instituicoes = InstituicaoEnsino.objects.all()
         return render(request, 'instituicao.html', {'instituicoes': instituicoes})
 
-class AreasSaberView(View):
-    def get(self, request, *args, **kwargs):
-        areas = AreaSaber.objects.all()
-        return render(request, 'area_saber.html', {'areas': areas})
-
-class CursosView(View):
-    def get(self, request, *args, **kwargs):
-        cursos = Curso.objects.all()
-        return render(request, 'curso.html', {'cursos': cursos})
-
-class TurmasView(View):
-    def get(self, request, *args, **kwargs):
-        turmas = Turma.objects.all()
-        return render(request, 'turma.html', {'turmas': turmas})
-
 class DisciplinasView(View):
     def get(self, request, *args, **kwargs):
         disciplinas = Disciplina.objects.all()
         return render(request, 'disciplina.html', {'disciplinas': disciplinas})
-
-class PeriodosView(View):
-    def get(self, request, *args, **kwargs):
-        periodos = Periodo.objects.all()
-        return render(request, 'periodo.html', {'periodos': periodos})
-
-class CursoDisciplinasView(View):
-    def get(self, request, *args, **kwargs):
-        curso_disciplinas = CursoDisciplina.objects.all()
-        return render(request, 'curso_disciplina.html', {'curso_disciplinas': curso_disciplinas})
-
-class TurnosView(View):
-    def get(self, request, *args, **kwargs):
-        turnos = Turno.objects.all()
-        return render(request, 'turno.html', {'turnos': turnos})
-
-class MatriculasView(View):
-    def get(self, request, *args, **kwargs):
-        matriculas = Matricula.objects.all()
-        return render(request, 'matricula.html', {'matriculas': matriculas})
-
-class AvaliacaoTiposView(View):
-    def get(self, request, *args, **kwargs):
-        tipos = AvaliacaoTipo.objects.all()
-        return render(request, 'avaliacao_tipo.html', {'tipos': tipos})
-
-class AvaliacoesView(View):
-    def get(self, request, *args, **kwargs):
-        avaliacoes = Avaliacao.objects.all()
-        return render(request, 'avaliacao.html', {'avaliacoes': avaliacoes})
 
 class FrequenciasView(View):
     def get(self, request, *args, **kwargs):
         frequencias = Frequencia.objects.all()
         return render(request, 'frequencia.html', {'frequencias': frequencias})
 
+class AvaliacoesView(View):
+    def get(self, request, *args, **kwargs):
+        avaliacoes = Avaliacao.objects.all()
+        return render(request, 'avaliacao.html', {'avaliacoes': avaliacoes})
+
+class TurmasView(View):
+    def get(self, request, *args, **kwargs):
+        turmas = Turma.objects.all()
+        return render(request, 'turma.html', {'turmas': turmas})
+
 class OcorrenciasView(View):
     def get(self, request, *args, **kwargs):
         ocorrencias = Ocorrencia.objects.all()
         return render(request, 'ocorrencia.html', {'ocorrencias': ocorrencias})
+    
--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import TemplateView
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
-    path('cidade/', CidadesView.as_view(), name='cidade'),
-    path('ocupacao/', OcupacoesView.as_view(), name='ocupacao'),
     path('pessoa/', PessoasView.as_view(), name='pessoa'),
-    path('instituicao/', InstituicoesView.as_view(), name='instituicao'),
-    path('area_saber/', AreasSaberView.as_view(), name='area_saber'),
-    path('curso/', CursosView.as_view(), name='curso'),
-    path('turma/', TurmasView.as_view(), name='turma'),
-    path('disciplina/', DisciplinasView.as_view(), name='disciplina'),
-    path('periodo/', PeriodosView.as_view(), name='periodo'),
-    path('curso_disciplina/', CursoDisciplinasView.as_view(), name='curso_disciplina'),
-    path('turno/', TurnosView.as_view(), name='turno'),
     path('matricula/', MatriculasView.as_view(), name='matricula'),
-    path('avaliacao_tipo/', AvaliacaoTiposView.as_view(), name='avaliacao_tipo'),
+    path('instituicao/', InstituicoesView.as_view(), name='instituicao'),
+    path('curso/', CursosView.as_view(), name='curso'),
+    path('disciplina/', DisciplinasView.as_view(), name='disciplina'),
     path('avaliacao/', AvaliacoesView.as_view(), name='avaliacao'),
     path('frequencia/', FrequenciasView.as_view(), name='frequencia'),
+    path('turma/', TurmasView.as_view(), name='turma'),
     path('ocorrencia/', OcorrenciasView.as_view(), name='ocorrencia'),
 ]
+
